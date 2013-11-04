e375373e3af1b130a6e1b7470cc019c8db515802
Ryan Koesters <rmkoesters@gmail.com>
2013-11-04 00:03:21 -0600

    Revert to basic prompts.

--- .config/sh/shrc.d/prompt.sh
+++ .config/sh/shrc.d/prompt.sh
@@ -1,14 +1,3 @@
 PS1='$ '
 PS2='> '
 PS4='+ '
-
-
-# Some useful colors.
-txtgrn='\e[0;32m'
-txtylw='\e[0;33m'
-txtwht='\e[0;37m'
-
-
-# Uncomment to use a color prompt.
-#PS1='\[\e[1;32m\][\u@\h \W]\$\[\e[0m\] '
-PS1="$txtgrn[\u@\h $txtylw\W$txtgrn]$txtgrn\$$txtwht "
