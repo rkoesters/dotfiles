7ab0beec1522088e47931f9dfb18412b4122ce33
Ryan Koesters <rmkoesters@gmail.com>
2013-11-08 05:56:12 -0600

    Simplified prompt_command.sh.

--- .config/sh/shrc.d/prompt_command.sh
+++ .config/sh/shrc.d/prompt_command.sh
@@ -1,26 +1,10 @@
-# Runs before every prompt.
-prompt_command () {
-	case $TERM in
-		xterm*|rxvt*|st*|screen*)
-			set_xterm_title "$(get_title_string)"
-			;;
-		9term|dumb)
-			# We will get an error if we ever actually use a
-			# dumb terminal, but I think it is safe to assume
-			# that it is Acme or 9term.
-			awd
-			;;
-		*)
-			;;
-	esac
-}
-
-# Get string used for terminal title.
-get_title_string () {
-	#pwd=$(pwd | sed "s#$HOME#~#")
-	#
-	#echo "$USER@$HOSTNAME:$pwd"
-	pwd
-}
-
-PROMPT_COMMAND="prompt_command"
+case $TERM in
+	xterm*|rxvt*|screen*)
+		PROMPT_COMMAND='set_xterm_title "$PWD"'
+		;;
+	9term|dumb)
+		PROMPT_COMMAND='awd'
+		;;
+	*)
+		;;
+esac
