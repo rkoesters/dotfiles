0a8105ae3bca8e76b6500dcdaefdbbd62009daec
Ryan Koesters <rmkoesters@gmail.com>
2015-11-05 11:57:24 -0600

    Added alert alias.

--- .config/bash/bashrc.d/alias.sh
+++ .config/bash/bashrc.d/alias.sh
@@ -7,4 +7,9 @@ case "$termprog" in
 		alias tree='_tree'
 		;;
 esac
+
+# I never use openvt, would rather have open point to xdg-open.
 alias open="xdg-open"
+
+# Send a notification about the previous command.
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo utilities-terminal || echo dialog-error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
